681325ba96fa31aae12c098d6deefcea298b815a
18040183 <caoheyang@shinho.net.cn>
2019-12-24 22:01:40 +0800

    提交代码测试

--- vars/JavaLambdaCI.groovy
+++ vars/JavaLambdaCI.groovy
@@ -18,7 +18,7 @@ def call(body) {
       }
       stage('udpate lambda function') {
         def file_path= sh('pwd');
-        print(''python update_lambda.py -lambda_function tiny-url-lambda -'+file_path+'/target/tiny-url-1.0-SNAPSHOT-lambda-package.zip -profile global -region us-east-1'')
+        print('python update_lambda.py -lambda_function tiny-url-lambda -'+file_path+'/target/tiny-url-1.0-SNAPSHOT-lambda-package.zip -profile global -region us-east-1')
         def update_lambda_code = libraryResource 'update_lambda.py'
         writeFile file: 'update_lambda.py', text: update_lambda_code
         sh('python update_lambda.py -lambda_function tiny-url-lambda -'+file_path+'/target/tiny-url-1.0-SNAPSHOT-lambda-package.zip -profile global -region us-east-1')

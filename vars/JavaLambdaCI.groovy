c231905dddefa62e9102e3a61ffdae9a83528da4
18040183 <caoheyang@shinho.net.cn>
2019-12-24 21:56:18 +0800

    提交测试代码

--- vars/JavaLambdaCI.groovy
+++ vars/JavaLambdaCI.groovy
@@ -1,4 +1,12 @@
 def call(body) {
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  def jenkins_node = 'master'
+
+  def project = config.project
   pipeline {
     node {
       stage('chekcout source code') {
@@ -9,9 +17,11 @@ def call(body) {
         sh('pwd')
       }
       stage('udpate lambda function') {
+        def file_path= sh('pwd');
+        print('sssssssssssssss')
         def update_lambda_code = libraryResource 'update_lambda.py'
         writeFile file: 'update_lambda.py', text: update_lambda_code
-        sh('python update_lambda.py -lambda_function tiny-url-lambda -file_path target/tiny-url-1.0-SNAPSHOT-lambda-package.zip -profile global -region us-east-1')
+        sh('python update_lambda.py -lambda_function tiny-url-lambda -'+file_path+'/target/tiny-url-1.0-SNAPSHOT-lambda-package.zip -profile global -region us-east-1')
       }
     }
   }

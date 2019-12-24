b45a50b49e76bf417e3650cb0674c0c6b81daa58
18040183 <caoheyang@shinho.net.cn>
2019-12-24 19:15:28 +0800

    提交代码

--- vars/JavaLambdaCI.groovy
+++ vars/JavaLambdaCI.groovy
@@ -1,15 +1,23 @@
-pipeline {
-  agent { label master } 
-  stages {
+def call(body) {
+ def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+  pipeline {
+    agent {
+      label master
+    }
+    stages {
       stage('checkout code') {
-          steps {
-                checkout scm
-          }
+        steps {
+          checkout scm
+        }
       }
       stage('compile') {
-          steps {
-              sh 'mvn clean package'
-          }
+        steps {
+          sh 'mvn clean package'
+        }
       }
+    }
   }
-}  
+}

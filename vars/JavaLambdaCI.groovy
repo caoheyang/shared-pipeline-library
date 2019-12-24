072e10df041f55c495f781ace34cd6f6dc385cbf
18040183 <caoheyang@shinho.net.cn>
2019-12-24 14:20:32 +0800

    测试

--- vars/JavaLambdaCI.groovy
+++ vars/JavaLambdaCI.groovy
@@ -1,21 +1,12 @@
 def call(body){ 
-  pileline{
-    agent any
-    tools {
-        maven 'Maven_3.5.2' 
+  pipeline {
+    agent { docker 'maven:3-alpine' } 
+    stages {
+        stage('Example Build') {
+            steps {
+                sh 'mvn clean package'
+            }
+        }
     }
-    stages{
-      stage('chekcout source code'){
-        checkout scm
-      }
-      stage('compile'){
-        sh('mvn clean package')
-      }
-      stage('udpate lambda function'){
-        def update_lambda_code=libraryResource 'update_lambda.py'
-        writeFile file: 'update_lambda.py', text: update_lambda_code
-        sh('python update_lambda.py -lambda_function tiny-url-lambda -file_path target/tiny-url-1.0-SNAPSHOT-lambda-package.zip -profile global -region us-east-1')
-      }
-    }
-  }
+  }  
 }

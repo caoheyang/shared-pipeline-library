44a07689049f94d380517f8c7dc99c2713e5f4cd
18040183 <caoheyang@shinho.net.cn>
2019-12-24 19:21:59 +0800

    测试1

--- vars/JavaLambdaCI.groovy
+++ vars/JavaLambdaCI.groovy
@@ -1,23 +1,24 @@
 def call(body) {
-  def config = [:]
+  def config = [: ]
   body.resolveStrategy = Closure.DELEGATE_FIRST
   body.delegate = config
   body()
-  
+
   pipeline {
     agent {
       label master
     }
     stages {
-      stage('checkout code') {
-        steps {
-          checkout scm
-        }
+      stage('chekcout source code') {
+        checkout scm
       }
       stage('compile') {
-        steps {
-          sh 'mvn clean package'
-        }
+        sh('mvn clean package')
+      }
+      stage('udpate lambda function') {
+        def update_lambda_code = libraryResource 'update_lambda.py'
+        writeFile file: 'update_lambda.py', text: update_lambda_code
+        sh('python update_lambda.py -lambda_function tiny-url-lambda -file_path target/tiny-url-1.0-SNAPSHOT-lambda-package.zip -profile global -region us-east-1')
       }
     }
   }

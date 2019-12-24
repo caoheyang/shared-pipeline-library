4ed5fab2c595e49bfef00704bda82119d5bdb574
18040183 <caoheyang@shinho.net.cn>
2019-12-24 14:03:10 +0800

    测试agent

--- vars/JavaLambdaCI.groovy
+++ vars/JavaLambdaCI.groovy
@@ -1,10 +1,10 @@
-def call(body) {
+def call(body) 
   node {
     agent {
-        docker {
-            image 'maven:3-alpine' 
-            args '-v /root/.m2:/root/.m2' 
-        }
+      docker {
+          image 'maven:3-alpine' 
+          args '-v /root/.m2:/root/.m2' 
+      }
     }
     stage('chekcout source code'){
         checkout scm

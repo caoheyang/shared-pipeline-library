72907fcba2e6f4edd4484b425942dc1d269df24c
18040183 <caoheyang@shinho.net.cn>
2019-12-25 12:23:53 +0800

    测试输出动态

--- vars/JavaLambdaCI.groovy
+++ vars/JavaLambdaCI.groovy
@@ -1,6 +1,6 @@
 def call(body) {
   def config = [:]
-  body.resolveStrategy = Closure.DELEGATE_FIRST
+  resolveStrategy = Closure.DELEGATE_FIRST
   body.delegate = config
   body()
 
@@ -17,6 +17,9 @@ def call(body) {
         sh('pwd')
       }
       stage('udpate lambda function') {
+        print('--------测试输出---------')
+        print(resolveStrategy)
+        print(body.delegate)
         def update_lambda_code = libraryResource 'update_lambda.py'
         writeFile file: 'update_lambda.py', text: update_lambda_code
         sh('python3 update_lambda.py -lambda_function tiny-url-lambda -file_path /root/.jenkins/workspace/tiny-url-lambda_master/target/tiny-url-1.0-SNAPSHOT-lambda-package.zip -profile global -region us-east-1')

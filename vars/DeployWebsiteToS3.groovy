a7656459b096eee22927733a4e9c209f23438149
18040183 <caoheyang@shinho.net.cn>
2019-12-25 22:38:12 +0800

    提交deploywebsitetos3

---
+++ vars/DeployWebsiteToS3.groovy
@@ -0,0 +1,20 @@
+def call(body) {
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+  
+  def project = config.project
+  def jenkins_node = 'master'
+ 
+  pipeline {
+    node {
+      stage('chekcout source code') {
+        checkout scm
+      }
+      stage('deploy website to S3') {
+        sh('aws s3 sync . s3://+'+project+'/  --profile global --region us-east-1 --delete --exclude ".git/*"')
+      }
+    }
+  }
+}

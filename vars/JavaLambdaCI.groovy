df0db4b2529a99324e85661ad052d3db022e858a
caoheyang <328690901@qq.com>
2019-12-25 12:35:21 +0800

    Update JavaLambdaCI.groovy

--- vars/JavaLambdaCI.groovy
+++ vars/JavaLambdaCI.groovy
@@ -16,12 +16,10 @@ def call(body) {
         sh('mvn clean package')
       }
       stage('udpate lambda function') {
-        print('--------测试输出---------')
-        def path=pwd()
-        print(path)
+        def file_path=pwd()
         def update_lambda_code = libraryResource 'update_lambda.py'
         writeFile file: 'update_lambda.py', text: update_lambda_code
-        sh('python3 update_lambda.py -lambda_function tiny-url-lambda -file_path /root/.jenkins/workspace/tiny-url-lambda_master/target/tiny-url-1.0-SNAPSHOT-lambda-package.zip -profile global -region us-east-1')
+        sh('python3 update_lambda.py -lambda_function tiny-url-lambda -file_path '+file_path+'/target/tiny-url-1.0-SNAPSHOT-lambda-package.zip -profile global -region us-east-1')
       }
     }
   }

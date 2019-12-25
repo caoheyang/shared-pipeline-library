bb29e91cf8af0647f4f5b7b6d06240d64f670a40
caoheyang <328690901@qq.com>
2019-12-25 13:04:17 +0800

    Update JavaLambdaCI.groovy

--- vars/JavaLambdaCI.groovy
+++ vars/JavaLambdaCI.groovy
@@ -21,7 +21,7 @@ def call(body) {
         def file_path=pwd()
         def update_lambda_code = libraryResource 'update_lambda.py'
         writeFile file: 'update_lambda.py', text: update_lambda_code
-        sh('python3 update_lambda.py -lambda_function tiny-url-lambda -file_path '+file_path+'/target/tiny-url-1.0-SNAPSHOT-lambda-package.zip -profile global -region us-east-1')
+        sh('python3 update_lambda.py -lambda_function tiny-url-lambda -file_path '+file_path+'/target/'+project+'-lambda-package.zip -profile global -region us-east-1')
       }
     }
   }

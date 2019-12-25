b3c3757e599f4b4837cdb0bb4d25aa1f9d4cefb2
18040183 <caoheyang@shinho.net.cn>
2019-12-25 12:15:34 +0800

    使用python3

--- vars/JavaLambdaCI.groovy
+++ vars/JavaLambdaCI.groovy
@@ -19,7 +19,7 @@ def call(body) {
       stage('udpate lambda function') {
         def update_lambda_code = libraryResource 'update_lambda.py'
         writeFile file: 'update_lambda.py', text: update_lambda_code
-        sh('python update_lambda.py -lambda_function tiny-url-lambda -file_path /root/.jenkins/workspace/tiny-url-lambda_master/target/tiny-url-1.0-SNAPSHOT-lambda-package.zip -profile global -region us-east-1')
+        sh('python3 update_lambda.py -lambda_function tiny-url-lambda -file_path /root/.jenkins/workspace/tiny-url-lambda_master/target/tiny-url-1.0-SNAPSHOT-lambda-package.zip -profile global -region us-east-1')
       }
     }
   }

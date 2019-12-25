749b5963ee9db7a616a6d1e53949c53470c85132
18040183 <caoheyang@shinho.net.cn>
2019-12-25 12:13:16 +0800

    测试流水线代码

--- vars/JavaLambdaCI.groovy
+++ vars/JavaLambdaCI.groovy
@@ -4,7 +4,7 @@ def call(body) {
   body.delegate = config
   body()
 
-  def jenkins_node = 'slave'
+  def jenkins_node = 'master'
 
   def project = config.project
   pipeline {
@@ -17,11 +17,9 @@ def call(body) {
         sh('pwd')
       }
       stage('udpate lambda function') {
-        def file_path= sh('pwd');
-        print('python update_lambda.py -lambda_function tiny-url-lambda -'+file_path+'/target/tiny-url-1.0-SNAPSHOT-lambda-package.zip -profile global -region us-east-1')
         def update_lambda_code = libraryResource 'update_lambda.py'
         writeFile file: 'update_lambda.py', text: update_lambda_code
-        sh('python update_lambda.py -lambda_function tiny-url-lambda -'+file_path+'/target/tiny-url-1.0-SNAPSHOT-lambda-package.zip -profile global -region us-east-1')
+        sh('python update_lambda.py -lambda_function tiny-url-lambda -file_path /root/.jenkins/workspace/tiny-url-lambda_master/target/tiny-url-1.0-SNAPSHOT-lambda-package.zip -profile global -region us-east-1')
       }
     }
   }
